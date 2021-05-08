f3f3f5b7e2b2ef6b5bd98204bfd9cd89722763e0
Chayapon99 <55127411+Chayapon99@users.noreply.github.com>
2021-05-08 19:20:32 +0700

    first commit

---
+++ final-game/Assets/Scripts/Coin.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Coin : MonoBehaviour {
+
+    [SerializeField] float turnSpeed = 90f;
+    public AudioClip CoinSound;
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.GetComponent<Obstacle>() != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Check that the object we collided with is the player
+        if (other.gameObject.name != "Catrina") {
+            return;
+        }
+
+        // Add to the player's score
+        GameManager.inst.IncrementScore();
+
+        // Destroy this coin object
+        AudioSource.PlayClipAtPoint(CoinSound, transform.position);
+        Destroy(gameObject);
+    }
+
+    private void Start () {
+
+	}
+
+	private void Update () {
+        transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
+	}
+}
---
+++ final-game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+
+    public void LoadMainMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
