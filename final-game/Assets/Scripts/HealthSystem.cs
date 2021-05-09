7f37348720e605ae1bdb9fb9b9f36db96ead45c8
Chayapon99 <55127411+Chayapon99@users.noreply.github.com>
2021-05-09 15:39:59 +0700

    added hearts UI

---
+++ final-game/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    public int life;
+
+
+    public void TakeDamage()
+    {
+        if (life >= 1)
+        {
+            life -= 1;
+            Destroy(hearts[life].gameObject);
+            if (life < 1)
+            {
+                
+            }
+        }
+    }
+}
